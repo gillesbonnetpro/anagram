9630b8bac3acd1db9b3c83412321088e3d82512a
GB <gilles.bonnet.pro@gmail.com>
2023-12-30 15:47:46 +0100

    pastille animées

--- anagram/lib/pastille.dart
+++ anagram/lib/pastille.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class Pastille extends StatelessWidget {
   final String lettre;
   final Color color;
+  bool animated;
 
-  const Pastille({super.key, required this.lettre, required this.color});
+  Pastille(
+      {super.key,
+      required this.lettre,
+      required this.color,
+      required this.animated});
+
+  TextStyle style = const TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.w500,
+      fontSize: 50,
+      decoration: TextDecoration.none);
 
   @override
   Widget build(BuildContext context) {
-    TextStyle style = const TextStyle(
-        color: Colors.white,
-        fontWeight: FontWeight.w500,
-        fontSize: 50,
-        decoration: TextDecoration.none);
-    return Padding(
+    Widget pastille = Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
         width: 50,
@@ -32,5 +39,12 @@ class Pastille extends StatelessWidget {
         ),
       ),
     );
+
+    if (animated) {
+      animated = false;
+      return pastille.animate().scale(duration: 500.ms);
+    } else {
+      return pastille;
+    }
   }
 }
